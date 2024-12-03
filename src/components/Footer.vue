218064395191c8a232946cc43e0c00e7e4b43c61
litauris <litauris.reg@gmail.com>
2024-12-04 00:11:21 +0100

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <div class="row wrapper">
       <h2>What are you waiting for?</h2>
-      <h3>Get a high-quality, user-friendly App<span> within the next month</span></h3>
+      <h3>Get a high quality, user-friendly App<span> within the next month</span></h3>
       <button @click="visible = true">
         <span>Get an instant quote</span>
       </button>
